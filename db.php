4a5a139fc96d54caf93efdf179ccf636fa7f338d
Hamza ORTATEPE <hortatepe2002@gmail.com>
2023-02-17 17:23:03 +0300

    fix create

--- db.php
+++ db.php
@@ -4,10 +4,11 @@
     $password           = "";
     $dbname             = "kayit";
 try {
-    $conn               = mysqli_connect($servername, $username, $password, $dbname);
+    $conn               =   mysqli_connect($servername, $username, $password);
     $databaseCreate     =   mysqli_query($conn,"CREATE DATABASE IF NOT EXISTS $dbname");
     $useDB              =   mysqli_query($conn,"USE $dbname");
     $tableCreate        =   mysqli_query($conn,"CREATE TABLE IF NOT EXISTS users (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, email VARCHAR(30) NOT NULL, pass VARCHAR(30) NOT NULL)");
+    echo "<script>alert('Database bağlantısı başarılı')</script>";
 } catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
 }
