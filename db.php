b0a399ce8227ab372060b560d534910bad753e70
Hamza ORTATEPE <hortatepe2002@gmail.com>
2023-02-17 17:16:34 +0300

    db create

--- db.php
+++ db.php
@@ -1,10 +1,13 @@
 <?php
+    $servername         = "localhost";
+    $username           = "root";
+    $password           = "";
+    $dbname             = "kayit";
 try {
-    $servername = "localhost";
-    $username   = "root";
-    $password   = "";
-    $dbname     = "kayit";
-    $conn   = mysqli_connect($servername, $username, $password, $dbname);
+    $conn               = mysqli_connect($servername, $username, $password, $dbname);
+    $databaseCreate     =   mysqli_query($conn,"CREATE DATABASE IF NOT EXISTS $dbname");
+    $useDB              =   mysqli_query($conn,"USE $dbname");
+    $tableCreate        =   mysqli_query($conn,"CREATE TABLE IF NOT EXISTS users (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, email VARCHAR(30) NOT NULL, pass VARCHAR(30) NOT NULL)");
 } catch (Exception $e) {
     echo "Connection failed: " . $e->getMessage();
 }
